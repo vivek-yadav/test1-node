328734bfdfba3a49cfbcce37376dd7593a9a3262
vivek1 <vivek1@MacBook-Pro-2.local>
2019-02-05 12:26:07 +0530

    updated with docker file

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-var http = require("http");
+'use strict';
 
-var version = process.env.VERSION || "1.0"
-var desc = process.env.DESC || "pre-interview technical test"
-var lastcommitsha = process.env.LAST_COMMIT_SHA || "SHA***---***"
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8081;
+const HOST = process.env.HOST || '0.0.0.0';
+
+var version = process.env.VERSION || "1.0";
+var desc = process.env.DESC || "pre-interview technical test";
+var lastcommitsha = process.env.LAST_COMMIT_SHA || "SHA***---***";
 
 var healthcheck = {
     "myapplication": [
@@ -14,16 +20,13 @@ var healthcheck = {
     ]
 }
 
-var express = require('express');
-var app = express();
+// App
+const app = express();
+app.get('/healthcheck', (req, res) => {
+  res.send(JSON.stringify(healthcheck));
+});
 
-app.get('/healthcheck', function (req, res) {
-   res.send(JSON.stringify(healthcheck));
-})
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+
